00c91da528aae4edb3337c11f081a84d4ac8eeaf
unknown <aranzaiortegas@gmail.com>
2020-12-09 16:15:10 +0100

    first change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          HEY! I'M ARANZA ORTEGA, JUNIOR FRONTEND DEVELOPER
         </p>
         <a
           className="App-link"
