04209ac010393e8725bc91663050414b0cd0eced
Aranza <aranzaiortegas@gmail.com>
2020-12-15 14:55:30 +0100

    deleting default react styles and adding principal sidebar component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import ThemeSwitcher from './First.js';
+import React from 'react';
+import "./App.css";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          HEY! I'M ARANZA ORTEGA, JUNIOR FRONTEND DEVELOPER
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <ThemeSwitcher/>
+        <Sidebar/>
     </div>
   );
 }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../App.css";
+
+function Sidebar() {
+    return (
+        <div className="Sidebar">
+            sajdhsad
+        </div>
+    )
+}
+
+export default Sidebar
